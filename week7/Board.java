54ff858cad198f7a8b7c7589cc6607cf3ffee31a
BerraKoc <kocberra3@gmail.com>
2025-11-06 20:52:26 +0300

    week7.1

---
+++ week7/Board.java
@@ -0,0 +1,26 @@
+package msku.ceng.madlab.week7;
+
+public class Board {
+
+    static final byte PLAYER_1_SYMBOL = 'X';
+    static final byte PLAYER_2_SYMBOL = 'O';
+    boolean player1Turn = true;
+    byte[][]board = new byte[3][3];
+    BoardListener boardListener;
+    public Board (BoardListener boardListener){
+        this.boardListener = boardListener;
+    }
+
+    public void move(byte row, byte col){
+        if(player1Turn){
+            board[row][col] = PLAYER_1_SYMBOL;
+            boardListener.playedAt(BoardListener.PLAYER_1, row, col);
+        }
+        else{
+            board[row][col] = PLAYER_2_SYMBOL;
+            boardListener.playedAt(BoardListener.PLAYER_2, row, col);
+        }
+        player1Turn =! player1Turn;
+    }
+
+}
